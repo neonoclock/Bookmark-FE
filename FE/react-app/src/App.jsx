a509e6d17ba8bd5e53eac5797a060dff42b94558
neonoclock <neonoclock213@naver.com>
2026-03-02 14:54:32 +0900

    feat(ui): 보드 페이지 UI 개선

--- FE/react-app/src/App.jsx
+++ FE/react-app/src/App.jsx
@@ -1,16 +1,10 @@
 import AppLayout from "./app/AppLayout.jsx";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <AppLayout>
-      <nav className="app-nav">
-        <NavLink to="/board">게시글 목록</NavLink>
-        <NavLink to="/login">로그인</NavLink>
-        <NavLink to="/signup">회원가입</NavLink>
-        <NavLink to="/profile-edit">프로필 수정</NavLink>
-      </nav>
       <Outlet />
     </AppLayout>
   );
